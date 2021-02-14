c3613806410e0a641e561f715031e42b0bc30e71
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-14 20:44:26 +0100

    use useEffect hook to fetch data from the db.json file

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,8 +1,24 @@
 import CourseList from "../courses/CourseList";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+    const [courses, setCourses] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/courses')
+            .then(res => res.json())
+            .then(data => {
+                setCourses(data);
+                setIsPending(false);
+            })
+    }, []);
+
     return ( 
-        <CourseList />
+        <>
+            { isPending && <p>Loading...</p> }
+            { courses && <CourseList courses={courses} /> }
+        </>
      );
 }
  
