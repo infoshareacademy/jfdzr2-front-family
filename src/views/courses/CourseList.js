28d6302edffb3567aaf13ea6dc4e38effe281e99
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-14 21:14:19 +0100

    add the content to the JSX template in the CourseList component using data passed in the props destructured as courses; however, the data does appear on the page but it looks like they exist when they are logged into the console

--- src/views/courses/CourseList.js
+++ src/views/courses/CourseList.js
@@ -1,6 +1,28 @@
-const CourseList = () => {
+const CourseList = ({ courses }) => {
     return ( 
-        null
+        <div className="course__list">
+            {courses.map(course => {
+                <div className="course__tile">
+                    <div className="course__image">
+                        <img src={ course.image }/>
+                    </div>
+                    <div className="course__info">
+                        <h2>{ course.title }</h2>
+                        <p>{ course.summary }</p>
+                        <p>{ course.category }</p>
+                        <p>{ course.author }</p>
+                        <p>{ course.rating }</p>
+                        <div className="course__time__and__level">
+                            <p>{ course.duration }</p>
+                            <p>{ course.level }</p>
+                        </div>
+                    </div>
+                    <div className="course__price">
+                        <p>{ course.price }</p>
+                    </div>
+                </div>
+            })}
+        </div>
      );
 }
  
