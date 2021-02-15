1370add1928af3c7a548d792ac5160e872169b69
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-15 21:04:44 +0100

    add catch error to the useEffect hook when fetching data to signal to the user that the data could not be fetched, e.g. when the url is incorrect

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -4,18 +4,31 @@ import { useEffect, useState } from "react";
 const Home = () => {
     const [courses, setCourses] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:8000/courses')
-            .then(res => res.json())
+            .then(res => {
+                if (res.status !== 200) {
+                    throw Error('Failed to fetch the data for that resource :(')
+                } else {
+                    return res.json()
+                }
+            })
             .then(data => {
                 setCourses(data);
                 setIsPending(false);
+                setError(null);
             })
+            .catch(err => {
+                setError(err.message);
+                setIsPending(false);
+            });
     }, []);
 
     return ( 
         <>
+            { error && <p>Failed to fetch the data for that resource :( </p> }
             { isPending && <p>Loading...</p> }
             { courses && <CourseList courses={courses} title="Our Courses" /> }
         </>
