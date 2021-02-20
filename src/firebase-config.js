6b330b9636de071f2f3aea0c5281c3abcc6d06f2
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-20 22:15:42 +0100

    move the firebase configuration to a separate file and make an import of this configuration in index.js

---
+++ src/firebase-config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCprGadwK9Gqzd5P4_kllO7WCp4TwOE6cg",
+    authDomain: "e-commerce-app-51342.firebaseapp.com",
+    databaseURL: "https://e-commerce-app-51342-default-rtdb.europe-west1.firebasedatabase.app/",
+    projectId: "e-commerce-app-51342",
+    storageBucket: "e-commerce-app-51342.appspot.com",
+    messagingSenderId: "862294770819",
+    appId: "1:862294770819:web:a80a4cbe6f358bd70b47b6"
+}
+
+export const DATABASE_URL = firebaseConfig.databaseURL
