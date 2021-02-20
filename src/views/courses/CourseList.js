77f35420a21e9e9c3534efe99c23d3b8fb8d5ca9
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-20 22:30:30 +0100

    move the content of the CourseList component  which is after the map function to a separate component that is to a CourseItem component

--- src/views/courses/CourseList.js
+++ src/views/courses/CourseList.js
@@ -1,33 +1,15 @@
 import "./CourseList.css";
-import { GoCalendar } from "react-icons/go";
-import { GrSteps } from "react-icons/gr";
-import { BsStarFill, BsStar, BsStarHalf } from 'react-icons/bs';
-import { VscFolderOpened } from "react-icons/vsc";
+import CourseItem from "./CourseItem";
 
 const CourseList = ({ courses, title }) => {
     return ( 
         <div className="course__list">
             <h2>{ title }</h2>
             {courses.map(course => (
-                <div className="course__tile" key={course.id}>
-                    <div className="course__image">
-                        <img src={ course.image } alt="course image" />
-                    </div>
-                    <div className="course__info">
-                        <h3>{ course.title }</h3>
-                        <p className="course__info__summary">{ course.summary }</p>
-                        <p className="course__info__category"><VscFolderOpened /> { course.category }</p>
-                        <p className="course__info__author">by { course.author }</p>
-                        <p className="course__info__rating">{ course.rating } <BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStar/></p>
-                        <div className="course__info__time__and__level">
-                            <p><GoCalendar /> { course.duration }h</p>
-                            <p><GrSteps /> { course.level }</p>
-                        </div>
-                    </div>
-                    <div className="course__info__price">
-                        <p>{ course.price }</p>
-                    </div>
-                </div>
+                <CourseItem 
+                    key={course.id}
+                    course={course}
+                />
             ))}
         </div>
      );
