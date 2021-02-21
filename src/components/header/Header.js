2e16a689c3c1c8153d8b317c70e54c08e0c49759
Weronika Kansy <weronika.kansy@gmail.com>
2021-02-21 13:08:53 +0100

    JSX fragment replaced with <header> in Header.js; Header.css added and imported in Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,11 @@
 import { Navigation } from "./Navigation";
 import { IntroSection } from "./IntroSection";
 
+import "./Header.css";
+
 export const Header = () => {
-  return <>
+  return <header className="app__header">
     <Navigation />
     <IntroSection />
-  </>
+  </header>
 }
