8514353ff789b27ca4da35122a544b81d67f70a9
Weronika Kansy <weronika.kansy@gmail.com>
2021-02-21 17:18:54 +0100

    Navigation links wrapped in divs; Navigation.css added and imported in Navigation.js

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -1,8 +1,14 @@
+import "./Navigation.css";
+
 export const Navigation = () => {
-  return <nav>
-    <a href="#">Future Skills</a>
-    <a href="#">Log in</a>
-    <a href="#">Sign up</a>
-    <a href="#">Cart</a>
+  return <nav className="header__nav">
+    <div>
+      <a href="#">Future Skills</a>
+    </div>
+    <div>
+      <a href="#">Log in</a>
+      <a href="#">Sign up</a>
+      <a href="#">Cart</a>
+    </div>
   </nav>
 }
