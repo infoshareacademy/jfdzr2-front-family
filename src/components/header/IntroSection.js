fbd86cb80bc1c4bcd5c08847dbbf0b87a168e7aa
Weronika Kansy <weronika.kansy@gmail.com>
2021-02-21 14:43:34 +0100

    IntroSection.css added and imported in IntroSection.js; IntroSection content updated

--- src/components/header/IntroSection.js
+++ src/components/header/IntroSection.js
@@ -1,5 +1,8 @@
+import "./IntroSection.css";
+
 export const IntroSection = () => {
-  return <section>
-    <h1>Lorem ipsum</h1>
+  return <section className="header__section">
+    <h1>Your future is now</h1>
+    <h2>Learn programming, web development, graphic design, languages, and more!</h2>
   </section>
 }
