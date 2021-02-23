c3c5fd9e85f3c6a8008e98e196d0f27656b8b0a6
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-23 19:21:21 +0100

    add import of the firebase firestore; modify the function for fetching data so it fetches the data from the firestore instead of realtime database; delete the state for displaying an error as had problems with applying it in a modified function

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,36 +1,21 @@
 import CourseList from "../courses/CourseList";
 import { useEffect, useState } from "react";
-import { DATABASE_URL } from "../../firebase-config";
+import { db } from '../../firebase-config';
 
 const Home = () => {
     const [courses, setCourses] = useState(null);
     const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch(`${DATABASE_URL}/courses.json`)
-            .then(res => {
-                if (res.status !== 200) {
-                    throw Error('Failed to fetch the data for that resource :(')
-                } else {
-                    return res.json()
-                }
-            })
-            .then(data => {
-                const formattedData = Object.keys(data).map(key => ({ id: key, ...data[key]}));
-                setCourses(formattedData);
+        db.collection('courses').get()
+            .then(snapshot => {
+                setCourses(snapshot.docs.map(doc => ({id: doc.id, ...doc.data()})))
                 setIsPending(false);
-                setError(null);
             })
-            .catch(err => {
-                setError(err.message);
-                setIsPending(false);
-            });
     }, []);
 
     return ( 
         <>
-            { error && <p>{ error }</p> }
             { isPending && <p>Loading...</p> }
             { courses && <CourseList courses={courses} title="Our Courses" /> }
         </>
