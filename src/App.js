6769f79e0dd76fb64fbcdbde49cbc8b0630034db
Malgorzata Prucnal <malgorzataprucnal@gmail.com>
2021-03-05 00:28:24 +0100

    Search bar added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../src/views/home/Home';
 import { Header } from "./components/header/Header";
 import { Footer } from "./components/footer/Footer";
+import { SearchBar } from "./components/searchbar/SearchBar";
+import { Navigation } from "./components/header/Navigation";
 
 function App() {
   return (
       <Router>
         <Route>
           <Header />
+          <SearchBar />
           <Switch>
             <Route exact path="/" component={Home} />
           </Switch>
