5ef25f4682e805bd74cbf02bafd44ed6ac5d72b0
Weronika Kansy <weronika.kansy@gmail.com>
2021-03-07 00:32:12 +0100

    assets/img folder with logo svg added to /src; logo embedded in navlink

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -1,11 +1,13 @@
 import { NavLink } from 'react-router-dom';
 
+import logoBlack from "../../assets/img/logo_black.svg";
+
 import "./Navigation.css";
 
 export const Navigation = () => {
   return <nav className="header__nav">
-    <div>
-      <NavLink exact to="/"></NavLink>
+    <div className="nav__logo__container">
+      <NavLink exact to="/"><img width="100px" src={logoBlack} alt="Future Skills" /></NavLink>
     </div>
     <div>
       <NavLink to="/log-in">Log in</NavLink>
