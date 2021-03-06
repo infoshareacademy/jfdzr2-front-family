84f627a8732ccb0f2ba0128ecac27c4ac9c3c1ec
Weronika Kansy <weronika.kansy@gmail.com>
2021-03-07 00:45:31 +0100

    navbar brought to front with z-index and fixed to top on scroll with useState and className toggle; other Navigation styles adjusted

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -1,11 +1,24 @@
 import { NavLink } from 'react-router-dom';
+import { useState } from "react";
 
 import logoBlack from "../../assets/img/logo_black.svg";
 
 import "./Navigation.css";
 
 export const Navigation = () => {
-  return <nav className="header__nav">
+  const [scroll, setScroll] = useState(false);
+
+  const handleScroll = () => {
+    if(window.scrollY > 60){
+      setScroll(true);
+    } else {
+      setScroll(false);
+    }
+  }
+
+  window.addEventListener("scroll", handleScroll);
+
+  return <nav className={scroll ? "header__nav nav--active" : "header__nav"}>
     <div className="nav__logo__container">
       <NavLink exact to="/"><img width="100px" src={logoBlack} alt="Future Skills" /></NavLink>
     </div>
