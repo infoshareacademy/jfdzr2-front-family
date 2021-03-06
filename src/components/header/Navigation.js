54ccfd4db3aff1a26ab4d5fa2b6a5a3122b65397
Weronika Kansy <weronika.kansy@gmail.com>
2021-03-07 00:20:23 +0100

    paths added to navlinks in Navigation

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -5,12 +5,12 @@ import "./Navigation.css";
 export const Navigation = () => {
   return <nav className="header__nav">
     <div>
-      <NavLink to="/">Future Skills</NavLink>
+      <NavLink exact to="/"></NavLink>
     </div>
     <div>
-      <NavLink to="/">Log in</NavLink>
-      <NavLink to="/">Sign up</NavLink>
-      <NavLink to="/">Cart</NavLink>
+      <NavLink to="/log-in">Log in</NavLink>
+      <NavLink to="/sign-up">Sign up</NavLink>
+      <NavLink to="/cart">Cart</NavLink>
     </div>
   </nav>
 }
