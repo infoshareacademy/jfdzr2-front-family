a03ab33c7ab9f6bd162e49893025ca85f83416dc
Weronika Kansy <weronika.kansy@gmail.com>
2021-03-07 20:27:07 +0100

    missing tag added in CourseItem

--- src/views/courses/CourseItem.js
+++ src/views/courses/CourseItem.js
@@ -6,29 +6,31 @@ import { VscFolderOpened } from "react-icons/vsc";
 import { Link } from "react-router-dom";
 
 const CourseItem = ({ course }) => {
-    return ( 
-        <div className="course__tile">
-            <div className="course__image">
-                <img src={ course.image } alt="course image" />
-            </div>
-            <div className="course__info">
-                <h3>{ course.title }</h3>
-                <p className="course__info__summary">{ course.summary }</p>
-                <p className="course__info__category"><VscFolderOpened /> { course.category }</p>
-                <p className="course__info__author">by { course.author }</p>
-                <div style={{display: "flex"}}>
-                    <p className="course__info__rating">{ course.rating }</p>
-                    {course.rating > 4 ? 
-                        (<p className="course__info__rating" style={{marginLeft: "5px"}}><BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarHalf/></p>) :
-                        (<p className="course__info__rating" style={{marginLeft: "5px"}}><BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarHalf /> <BsStar/></p>)
-                    }
+    return (
+        <Link to="/course-details"> 
+            <div className="course__tile">
+                <div className="course__image">
+                    <img src={ course.image } alt="course image" />
                 </div>
-                <div className="course__info__time__and__level">
-                    <p><GoCalendar /> { course.duration }h</p>
-                    <p><GrSteps /> { course.level }</p>
+                <div className="course__info">
+                    <h3>{ course.title }</h3>
+                    <p className="course__info__summary">{ course.summary }</p>
+                    <p className="course__info__category"><VscFolderOpened /> { course.category }</p>
+                    <p className="course__info__author">by { course.author }</p>
+                    <div style={{display: "flex"}}>
+                        <p className="course__info__rating">{ course.rating }</p>
+                        {course.rating > 4 ? 
+                            (<p className="course__info__rating" style={{marginLeft: "5px"}}><BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarHalf/></p>) :
+                            (<p className="course__info__rating" style={{marginLeft: "5px"}}><BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarHalf /> <BsStar/></p>)
+                        }
+                    </div>
+                    <div className="course__info__time__and__level">
+                        <p><GoCalendar /> { course.duration }h</p>
+                        <p><GrSteps /> { course.level }</p>
+                    </div>
                 </div>
             </div>
-        </Link>  
+        </Link>
      );
 }
  
