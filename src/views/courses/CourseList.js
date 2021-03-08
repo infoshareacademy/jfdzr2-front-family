5dc407ed8dd47011e3eb1cb35546691eb6447022
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-03-08 21:41:48 +0100

    in the CourseList component add the slice() method to show just four items on a page; in the Home component add a button for showing more courses and adjust the style and the position of this button on the page

--- src/views/courses/CourseList.js
+++ src/views/courses/CourseList.js
@@ -5,7 +5,7 @@ const CourseList = ({ courses, title }) => {
     return ( 
         <div className="course__list">
             <h2>{ title }</h2>
-            {courses.map(course => (
+            {courses.slice(0, 4).map(course => (
                 <CourseItem 
                     key={course.id}
                     course={course}
--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import { db } from '../../services/firebase-config';
 import SimpleAccordion from "../../components/sidebar/Sidebar";
 import "./Home.css";
+import Button from "@material-ui/core/Button";
+import { HiOutlineChevronDown } from "react-icons/hi";
 
 const Home = () => {
     const [courses, setCourses] = useState(null);
@@ -17,11 +19,26 @@ const Home = () => {
     }, []);
 
     return ( 
-        <div className="home__wrapper">
-            <SimpleAccordion />
-            { isPending && <p>Loading...</p> }
-            { courses && <CourseList courses={courses} title="Our Courses" /> }
-        </div>
+        <>
+            <div className="home__wrapper">
+                <SimpleAccordion />
+                { isPending && <p>Loading...</p> }
+                { courses && <CourseList courses={courses} title="Our Courses" /> }
+            </div>
+            <div className="load__more__btn__wrapper">
+                <Button variant="contained" color="inherit" 
+                    style={{
+                        backgroundColor: "#2EC4B6",
+                    }}
+                >Load more <HiOutlineChevronDown 
+                    style={{
+                        marginLeft: "10px",
+                        fontSize: "20px"
+                    }}
+                />
+                </Button>
+            </div>
+        </>
      );
 }
  
