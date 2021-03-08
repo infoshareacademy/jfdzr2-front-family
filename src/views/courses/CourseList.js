ba3e0b0b354fdd4de5eea3fb1d48cd49eafee8c6
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-03-08 22:15:25 +0100

    in the Home component: correc the initial state of the courses to an empty array instead of null, set another state called visibleCourses to use it in the function on a click event to laod more courses from the list and add the conditional rendering for the load more button to hide it when there are no more courses to laod; pass another prop (that is visibleCourses) to the CourseList component and output it in the JSX template

--- src/views/courses/CourseList.js
+++ src/views/courses/CourseList.js
@@ -1,11 +1,11 @@
 import "./CourseList.css";
 import CourseItem from "./CourseItem";
 
-const CourseList = ({ courses, title }) => {
+const CourseList = ({ courses, title, visibleCourses }) => {
     return ( 
         <div className="course__list">
             <h2>{ title }</h2>
-            {courses.slice(0, 4).map(course => (
+            {courses.slice(0, visibleCourses).map(course => (
                 <CourseItem 
                     key={course.id}
                     course={course}
--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -7,8 +7,9 @@ import Button from "@material-ui/core/Button";
 import { HiOutlineChevronDown } from "react-icons/hi";
 
 const Home = () => {
-    const [courses, setCourses] = useState(null);
+    const [courses, setCourses] = useState([]);
     const [isPending, setIsPending] = useState(true);
+    const [visibleCourses, setVisibleCourses] = useState(4);
 
     useEffect(() => {
         db.collection('courses').get()
@@ -18,25 +19,40 @@ const Home = () => {
             })
     }, []);
 
+    const handleShowMoreCourses = () => {
+        setVisibleCourses((previousValue) => previousValue + 4)
+    }
+
     return ( 
         <>
             <div className="home__wrapper">
                 <SimpleAccordion />
                 { isPending && <p>Loading...</p> }
-                { courses && <CourseList courses={courses} title="Our Courses" /> }
+                { courses && <CourseList 
+                    courses={courses} 
+                    title="Our Courses"
+                    visibleCourses={visibleCourses}
+                    /> 
+                }
             </div>
             <div className="load__more__btn__wrapper">
-                <Button variant="contained" color="inherit" 
+                { visibleCourses < courses.length ?
+                    (<Button variant="contained" color="inherit" 
                     style={{
                         backgroundColor: "#2EC4B6",
                     }}
-                >Load more <HiOutlineChevronDown 
-                    style={{
-                        marginLeft: "10px",
-                        fontSize: "20px"
-                    }}
-                />
-                </Button>
+                    onClick={handleShowMoreCourses}
+                    >
+                    Load more 
+                    <HiOutlineChevronDown 
+                        style={{
+                            marginLeft: "10px",
+                            fontSize: "20px"
+                        }}
+                    />
+                    </Button>) :
+                    null
+                }
             </div>
         </>
      );
