1d2801462f777d585989a277412bbce8c52d3d9c
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-03-12 23:41:56 +0100

    change the position of the Link component within the JSX template of the CourseItem component so it does not include the Add to Cart button as earlier when clicked on the button a user was redirected to the details page of a specific course; delete the pointer cursor property from the .course-tile selector so the pointer shows only on the elements wrapped in the Link component

--- src/views/courses/CourseItem.js
+++ src/views/courses/CourseItem.js
@@ -8,28 +8,29 @@ import { MdAddShoppingCart } from "react-icons/md";
 
 const CourseItem = ({ course }) => {
     return (
-        <Link to="/course-details"> 
-            <div className="course__tile">
+        <div className="course__tile">
                 <div className="course__image">
                     <img src={ course.image } alt="course image" />
                 </div>
-                <div className="course__info">
-                    <h3>{ course.title }</h3>
-                    <p className="course__info__summary">{ course.summary }</p>
-                    <p className="course__info__category"><VscFolderOpened /> { course.category }</p>
-                    <p className="course__info__author">by { course.author }</p>
-                    <div style={{display: "flex"}}>
-                        <p className="course__info__rating">{ course.rating }</p>
-                        {course.rating > 4 ? 
-                            (<p className="course__info__rating" style={{marginLeft: "5px"}}><BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarHalf/></p>) :
-                            (<p className="course__info__rating" style={{marginLeft: "5px"}}><BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarHalf /> <BsStar/></p>)
-                        }
+                <Link to="/course-details"> 
+                    <div className="course__info">
+                        <h3>{ course.title }</h3>
+                        <p className="course__info__summary">{ course.summary }</p>
+                        <p className="course__info__category"><VscFolderOpened /> { course.category }</p>
+                        <p className="course__info__author">by { course.author }</p>
+                        <div style={{display: "flex"}}>
+                            <p className="course__info__rating">{ course.rating }</p>
+                            {course.rating > 4 ? 
+                                (<p className="course__info__rating" style={{marginLeft: "5px"}}><BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarHalf/></p>) :
+                                (<p className="course__info__rating" style={{marginLeft: "5px"}}><BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarHalf /> <BsStar/></p>)
+                            }
+                        </div>
+                        <div className="course__info__time__and__level">
+                            <p><GoCalendar /> { course.duration }h</p>
+                            <p><GrSteps /> { course.level }</p>
+                        </div>
                     </div>
-                    <div className="course__info__time__and__level">
-                        <p><GoCalendar /> { course.duration }h</p>
-                        <p><GrSteps /> { course.level }</p>
-                    </div>
-                </div>
+                </Link>
                 <div className="course__info__price">
                     {typeof course.price === "number" ?
                         <p>{ course.price } PLN</p> :
@@ -45,7 +46,6 @@ const CourseItem = ({ course }) => {
                     </button>
                 </div>
             </div>
-        </Link>
      );
 }
  
