3a361cd431deba73b0bec13b70e49c41dd780009
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-03-14 21:16:44 +0100

    change the position of the Link component (again) in order to make the bottom border line visible , thus a new class name was added to the Link component and adjusted the style of it to make it responsive and make the add to cart button visible on hover

--- src/views/courses/CourseItem.js
+++ src/views/courses/CourseItem.js
@@ -14,48 +14,48 @@ const CourseItem = ({ course }) => {
     }
 
     return (
-        <Link to="/course-details"> 
-            <div className="course__tile"> 
-                <div className="course__image">
-                    <img src={ course.image } alt="course image" />
-                </div>
-                <div className="course__info">
-                    <h3>{ course.title }</h3>
-                    <p className="course__info__summary">{ course.summary }</p>
-                    <p className="course__info__category"><VscFolderOpened /> { course.category }</p>
-                    <p className="course__info__author">by { course.author }</p>
-                    <div style={{display: "flex"}}>
-                        <p className="course__info__rating">{ course.rating }</p>
-                            {course.rating > 4 ? 
-                                (<p className="course__info__rating" style={{marginLeft: "5px"}}><BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarHalf/></p>) :
-                                (<p className="course__info__rating" style={{marginLeft: "5px"}}><BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarHalf /> <BsStar/></p>)
+        <div className="course__tile"> 
+            <Link to="/course-details" className="course__details__link"> 
+                    <div className="course__image">
+                        <img src={ course.image } alt="course image" />
+                    </div>
+                    <div className="course__info">
+                        <h3>{ course.title }</h3>
+                        <p className="course__info__summary">{ course.summary }</p>
+                        <p className="course__info__category"><VscFolderOpened /> { course.category }</p>
+                        <p className="course__info__author">by { course.author }</p>
+                        <div style={{display: "flex"}}>
+                            <p className="course__info__rating">{ course.rating }</p>
+                                {course.rating > 4 ? 
+                                    (<p className="course__info__rating" style={{marginLeft: "5px"}}><BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarHalf/></p>) :
+                                    (<p className="course__info__rating" style={{marginLeft: "5px"}}><BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarHalf /> <BsStar/></p>)
+                                }
+                        </div>
+                        <div className="course__info__time__and__level">
+                            <p><GoCalendar /> { course.duration }h</p>
+                            <p><GrSteps /> { course.level }</p>
+                        </div>
+                    </div>
+                        <div className="course__info__price">
+                            {typeof course.price === "number" ?
+                                <p>{ course.price } PLN</p> :
+                                <p>{ course.price }</p>
                             }
-                    </div>
-                    <div className="course__info__time__and__level">
-                        <p><GoCalendar /> { course.duration }h</p>
-                        <p><GrSteps /> { course.level }</p>
-                    </div>
-                </div>
-                    <div className="course__info__price">
-                        {typeof course.price === "number" ?
-                            <p>{ course.price } PLN</p> :
-                            <p>{ course.price }</p>
-                        }
-                        <button 
-                            className="course__info__cart__btn" 
-                            title="Add to cart"
-                            onClick={(event) => handleAddToCart(event)}
-                        >
-                        <MdAddShoppingCart
-                            style={{
-                                paddingTop: "5px",
-                                fontSize: "25px"
-                                }}
-                        />
-                        </button>
-                    </div>
-            </div>
-        </Link>
+                            <button 
+                                className="course__info__cart__btn" 
+                                title="Add to cart"
+                                onClick={(event) => handleAddToCart(event)}
+                            >
+                            <MdAddShoppingCart
+                                style={{
+                                    paddingTop: "5px",
+                                    fontSize: "25px"
+                                    }}
+                            />
+                            </button>
+                        </div>
+            </Link>
+        </div>
      );
 }
  
