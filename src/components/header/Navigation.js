6af99c0c883a91dd2a46a2063cecd967aed6e0e7
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-03-15 18:19:36 +0100

    add the styled Badge component to the Navigation component to use it to show how many items are added to the shopping cart

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -1,10 +1,22 @@
 import { NavLink } from 'react-router-dom';
 import { useState } from "react";
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import Badge from '@material-ui/core/Badge';
+import { withStyles } from '@material-ui/core/styles';
 
 import logoBlack from "../../assets/img/logo_black.svg";
 
 import "./Navigation.css";
+
+const StyledBadge = withStyles((theme) => ({
+  badge: {
+    right: 2,
+    top: -1,
+    padding: '0 4px',
+    color: '#fff',
+    backgroundColor: '#2EC4B6'
+  },
+}))(Badge);
 
 export const Navigation = () => {
   const [scroll, setScroll] = useState(false);
@@ -26,7 +38,11 @@ export const Navigation = () => {
     <div className="header__nav__link__container">
       <NavLink to="/log-in">Log in</NavLink>
       <NavLink to="/sign-up">Sign up</NavLink>
-      <NavLink to="/cart"><ShoppingCartIcon /></NavLink>
+      <NavLink to="/cart">
+        <StyledBadge badgeContent={4}>
+          <ShoppingCartIcon />
+        </StyledBadge>
+      </NavLink>
     </div>
   </nav>
 }
