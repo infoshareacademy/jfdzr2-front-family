fdf29e2be0a45e272e108c1ff102e63f1fe2666d
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-03-16 17:07:53 +0100

    fix the hover effect on the cart icon in the navbar that disappeared earlier when moving the ShoppingCartIcon component to the CartViewPopper component

--- src/views/shopping-cart/CartViewPopper.js
+++ src/views/shopping-cart/CartViewPopper.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useState } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Popover from '@material-ui/core/Popover';
 import Typography from '@material-ui/core/Typography';
@@ -40,76 +41,88 @@ export default function CartViewPopper() {
     const open = Boolean(anchorEl);
     const id = open ? 'simple-popover' : undefined;
 
+    const [scroll, setScroll] = useState(false);
+
+    const handleScroll = () => {
+        if(window.scrollY > 60){
+        setScroll(true);
+        } else {
+        setScroll(false);
+        }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
     return (
-        <div>
-        <div aria-describedby={id} variant="contained" color="primary" onClick={handleClick}>
-            <StyledBadge badgeContent={4}>
-                <ShoppingCartIcon />
-            </StyledBadge>
-        </div>
-        <Popover
-            id={id}
-            open={open}
-            anchorEl={anchorEl}
-            onClose={handleClose}
-            anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'left',
-            }}
-            transformOrigin={{
-            vertical: 'top',
-            horizontal: 'center',
-            }}
-        >
-            <Typography 
-                className={classes.typography}
-                style={{
-                    maxWidth: "350px"
+        <div className={scroll ? "cart__popover__wrapper--active" : "cart__popover__wrapper"}>
+            <div aria-describedby={id} variant="contained" color="primary" onClick={handleClick} style={{padding: "18px 0"}}>
+                <StyledBadge badgeContent={4}>
+                    <ShoppingCartIcon />
+                </StyledBadge>
+            </div>
+            <Popover
+                id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+                }}
+                transformOrigin={{
+                vertical: 'top',
+                horizontal: 'center',
                 }}
             >
-                <div className="cart__view__list">
-                    <article className="cart__view__list__item">
-                        <div>
-                            <img src={CourseImage} alt="course image" />
-                        </div>
-                        <div>
-                            <p className="cart__view__list__item__title">Learn Business English</p>
-                            <p className="cart__view__list__item__author">by Jane White</p>
-                            <p>15.99 PLN</p>
-                        </div>
-                    </article>
-                    <article className="cart__view__list__item">
-                        <div>
-                            <img src={CourseImage} alt="course image" />
-                        </div>
-                        <div>
-                            <p className="cart__view__list__item__title">Agile Fundamentals: Including Scrum and Kanban</p>
-                            <p className="cart__view__list__item__author">by John O'Brien</p>
-                            <p>15.99 PLN</p>
-                        </div>
-                    </article>
-                    <article className="cart__view__list__item">
-                        <div>
-                            <img src={CourseImage} alt="course image" />
-                        </div>
-                        <div>
-                            <p className="cart__view__list__item__title">Complete Python - From Hero to Zero in Python</p>
-                            <p className="cart__view__list__item__author">by John Doe</p>
-                            <p>Free</p>
-                        </div>
-                    </article>
-                </div>
-                <div className="checkout__wrapper">
-                    <div className="checkout__total__price">
-                        <p>Total:</p>
-                        <p>28.98 PLN</p>
+                <Typography 
+                    className={classes.typography}
+                    style={{
+                        maxWidth: "350px"
+                    }}
+                >
+                    <div className="cart__view__list">
+                        <article className="cart__view__list__item">
+                            <div>
+                                <img src={CourseImage} alt="course image" />
+                            </div>
+                            <div>
+                                <p className="cart__view__list__item__title">Learn Business English</p>
+                                <p className="cart__view__list__item__author">by Jane White</p>
+                                <p>15.99 PLN</p>
+                            </div>
+                        </article>
+                        <article className="cart__view__list__item">
+                            <div>
+                                <img src={CourseImage} alt="course image" />
+                            </div>
+                            <div>
+                                <p className="cart__view__list__item__title">Agile Fundamentals: Including Scrum and Kanban</p>
+                                <p className="cart__view__list__item__author">by John O'Brien</p>
+                                <p>15.99 PLN</p>
+                            </div>
+                        </article>
+                        <article className="cart__view__list__item">
+                            <div>
+                                <img src={CourseImage} alt="course image" />
+                            </div>
+                            <div>
+                                <p className="cart__view__list__item__title">Complete Python - From Hero to Zero in Python</p>
+                                <p className="cart__view__list__item__author">by John Doe</p>
+                                <p>Free</p>
+                            </div>
+                        </article>
                     </div>
-                    <NavLink to="/cart" className="checkout__link">
-                        <button>Go to cart</button>
-                    </NavLink>
-                </div>
-            </Typography>
-        </Popover>
+                    <div className="checkout__wrapper">
+                        <div className="checkout__total__price">
+                            <p>Total:</p>
+                            <p>28.98 PLN</p>
+                        </div>
+                        <NavLink to="/cart" className="checkout__link">
+                            <button>Go to cart</button>
+                        </NavLink>
+                    </div>
+                </Typography>
+            </Popover>
         </div>
   );
 }
