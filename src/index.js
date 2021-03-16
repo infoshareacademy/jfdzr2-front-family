d57b24dd86226330e33a29384cb82e19f14271bc
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-03-16 19:51:10 +0100

    set up the Provider and create store to have global access to the reducers created earlier

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import './reset.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import allReducers from './reducers/all-reducers';
+
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
