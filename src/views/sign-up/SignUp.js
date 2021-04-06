0e6fe668287a4968642e7d6888d6108004aa451b
Weronika Kansy <weronika.kansy@gmail.com>
2021-04-06 14:25:44 +0200

    user email and password saved in firebase and removed from state on submit; attribute selectors changed to class selectors in CSS

--- src/views/sign-up/SignUp.js
+++ src/views/sign-up/SignUp.js
@@ -6,6 +6,8 @@ import { auth } from '../../services/firebase-config';
 import './SignUp.css';
 
 export const SignUp = () => {
+  const form = document.querySelector('.signup__form');
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -14,15 +16,21 @@ export const SignUp = () => {
 
   const handleOnSubmit = (event) => {
     event.preventDefault();
+    auth.createUserWithEmailAndPassword(email, password)
+      .then(() => {
+				// form.reset();
+        setEmail('');
+        setPassword('');
+      })
   };
 
-  return <form name="signUpForm" onSubmit={handleOnSubmit}>
+  return <form name="signUpForm" onSubmit={handleOnSubmit} className="signup__form">
     <h3>Sign up</h3>
     <label htmlFor="email">Email</label>
     <input id="email" type="email" value={email} name="email" onChange={handleOnEmailChange} required/>
     <label htmlFor="password">Password</label>
     <input id="password" type="password" value={password} name="password" onChange={handleOnPasswordChange} required/>
-    <input type="submit" value="Sign up"/>
+    <input type="submit" value="Sign up" />
     <p>Already have an account? <Link to="/log-in">Log in</Link></p>
   </form>
 }
