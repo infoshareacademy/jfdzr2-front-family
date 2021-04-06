c59d07da78849e31f8f4e7b9943e1c2768cc85f1
Weronika Kansy <weronika.kansy@gmail.com>
2021-04-06 03:31:51 +0200

    email and password saved in state with useState and onChange event handlers

--- src/views/sign-up/SignUp.js
+++ src/views/sign-up/SignUp.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import firebase from 'firebase';
 import { auth } from '../../services/firebase-config';
@@ -5,12 +6,18 @@ import { auth } from '../../services/firebase-config';
 import './SignUp.css';
 
 export const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleOnEmailChange = (event) => setEmail(event.target.value);
+	const handleOnPasswordChange = (event) => setPassword(event.target.value);
+
   return <form name="signUpForm">
     <h3>Sign up</h3>
     <label htmlFor="email">Email</label>
-    <input id="email" type="email" value="email" name="email" required/>
+    <input id="email" type="email" value={email} name="email" onChange={handleOnEmailChange} required/>
     <label htmlFor="password">Password</label>
-    <input id="password" type="password" value="password" name="password" required/>
+    <input id="password" type="password" value={password} name="password" onChange={handleOnPasswordChange} required/>
     <input type="submit" value="Sign up"/>
     <p>Already have an account? <Link to="/log-in">Log in</Link></p>
   </form>
