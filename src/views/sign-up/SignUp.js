25c08eb261770d11f16b732f5bd0c0ab9782a254
Weronika Kansy <weronika.kansy@gmail.com>
2021-04-06 03:23:05 +0200

    Link imported from react-router-dom; form content added

--- src/views/sign-up/SignUp.js
+++ src/views/sign-up/SignUp.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import firebase from 'firebase';
 import { auth } from '../../services/firebase-config';
 
@@ -6,5 +7,11 @@ import './SignUp.css';
 export const SignUp = () => {
   return <form name="signUpForm">
     <h3>Sign up</h3>
-  </form>;
+    <label htmlFor="email">Email</label>
+    <input id="email" type="email" value="email" name="email" required/>
+    <label htmlFor="password">Password</label>
+    <input id="password" type="password" value="password" name="password" required/>
+    <input type="submit" value="Sign up"/>
+    <p>Already have an account? <Link to="/log-in">Log in</Link></p>
+  </form>
 }
