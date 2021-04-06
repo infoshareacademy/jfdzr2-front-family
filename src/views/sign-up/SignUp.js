3c17bda29a2e755c7ba9f4f3a2d566c3e7f2621d
Weronika Kansy <weronika.kansy@gmail.com>
2021-04-06 03:06:00 +0200

    stylesheet added and imported in SignUp; form display set to flex

--- src/views/sign-up/SignUp.js
+++ src/views/sign-up/SignUp.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase';
 import { auth } from '../../services/firebase-config';
 
+import './SignUp.css';
+
 export const SignUp = () => {
   return <form name="signUpForm">
     <h3>Sign up</h3>
